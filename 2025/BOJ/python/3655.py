8fde10e68095173097e805161c4554199cf6b4f0
bavocadob <1ksy3629@gmail.com>
2025-04-13 00:05:59 +0900

    Feat : 20250412 3655 Python

--- 2025/BOJ/python/3655.py
+++ 2025/BOJ/python/3655.py
@@ -4,24 +4,27 @@ k = int(input())
 
 if k == 2:
     # 최소 합 3
-    print(r - max(l, 3) + 1)
+    print(max(0, (r - max(l, 3) + 1)))
 elif k == 3:
     # S = 3(x+d)
     # 최소 합 6
     start_val = max(l, 6)
-    count = (r // 3) - ((start_val - 1) // 3)
-    print(count)
+    if start_val > r:
+        print(0)
+    else:
+        count = (r // 3) - ((start_val - 1) // 3)
+        print(count)
 elif k == 4:
-    # S = 4x + 6d = 2(2x+3d)
+    # S = 2(2x+3d)수
     # 최소 합 10
-    start_val = max(l, 10)
-    count = (r // 2) - ((start_val - 1) // 2)
-    if l <= 12 <= r:
-        count -= 1
-    print(count)
+    # 14++ 모든짝
+    print(max(0, r // 2 - ((max(l, 14) - 1) // 2)) + (l <= 10 <= r))
 elif k == 5:
     # S = 5x + 10d = 5(x+2d)
     # 최소 합 15
     start_val = max(l, 15)
-    count = (r // 5) - ((start_val - 1) // 5)
-    print(count)
+    if start_val > r:
+        print(0)
+    else:
+        count = (r // 5) - ((start_val - 1) // 5)
+        print(count)
